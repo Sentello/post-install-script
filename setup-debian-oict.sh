797199b659e5bc71ad159e6264e2b7d0391945a6
Sentello <44606412+Sentello@users.noreply.github.com>
2022-08-01 15:51:46 +0300

    Update setup-debian-oict.sh

--- setup-debian-oict.sh
+++ setup-debian-oict.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# Spported OS: Debian 10, Ubuntu 20.04
+# Spported OS: Debian 10/11, Ubuntu 20.04
 # Post install script ver. 1.0
 # INSTRUCTIONS FOR USE:
 # 1. Copy this shell script to your /home directory or the /tmp directory.
